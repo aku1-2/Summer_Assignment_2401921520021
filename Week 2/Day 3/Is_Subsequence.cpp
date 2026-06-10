6471f140d2732cb2601c3b2a77a555de3243dfc8
Akanksha Giri <168823584+aku1-2@users.noreply.github.com>
2026-06-10 16:30:05 +0530

    Updated Is_Subsequence.cpp

--- Week 2/Day 3/Is_Subsequence.cpp
+++ Week 2/Day 3/Is_Subsequence.cpp
@@ -3,7 +3,7 @@ Given two strings s and t, return true if s is a subsequence of t, or false othe
 A subsequence of a string is a new string that is formed from the original string by deleting some (or none) of the characters without disturbing 
 the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
-Time Complexity:- O(N) at most t string tea. Best Case:- O(1) , Space Complexity:-O(1)
+Time Complexity:- O(N) at most t string size. Best Case:- O(1) , Space Complexity:-O(1)
 */
 class Solution {
 public:
