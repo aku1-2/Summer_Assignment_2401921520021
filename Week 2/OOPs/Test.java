bea8003bfc0da5662184678f7ebe53cbf68b2cce
Akanksha Giri <168823584+aku1-2@users.noreply.github.com>
2026-06-14 13:03:36 +0530

    Add files via upload

---
+++ Week 2/OOPs/Test.java
@@ -0,0 +1,22 @@
+package live;
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test{ 
+
+    public static void main(String[] args){
+
+        Veena v = new Veena();
+        Saxophone s = new Saxophone();
+        v.play();
+        s.play();
+
+        Playable p;
+
+        p = new Veena();
+        p.play();
+        p = new Saxophone();
+        p.play();
+    }
+}
