206b62b2c95e6c02c38c7f99b703519c0e76e0d6
Akanksha Giri <168823584+aku1-2@users.noreply.github.com>
2026-06-23 15:01:33 +0530

    Create Binary_Tree_Inorder_Traversal.cpp

---
+++ Week 4/Day 2/Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,19 @@
+/*
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+*/
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        inorder(root, result);
+        return result;
+    }
+    void inorder(TreeNode* root, vector<int>& result){
+        if(root==nullptr){
+            return;
+        }
+        inorder(root->left , result);
+        result.push_back(root->val);
+        inorder(root->right , result);
+    }
+};
